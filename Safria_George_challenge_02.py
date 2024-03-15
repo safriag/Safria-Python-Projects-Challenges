5645d927f7f3b51762471a1d5440df1a5371a434
safriag <163601088+safriag@users.noreply.github.com>
2024-03-15 17:54:51 -0500

    (Fall 2023) According to your birthday, you are...

---
+++ Safria_George_challenge_02.py
@@ -0,0 +1,6 @@
+user_name = input("Please enter your name: ")
+day = input("Please enter your DOB. \nDay: ")
+month = input("Month: ")
+year = int(input("Year: "))
+age = 2023 - year
+print(f"Hello {user_name}. According to your birthday, you are {age} years old.")
